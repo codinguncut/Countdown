5ffa96240ea55d3884cf42501d13aa57e8bc55e4
Jonas Tullus <entirelysubjective@googlemail.com>
2012-06-27 08:50:49 +1030

    initial checkin

---
+++ Countdown.hs
@@ -0,0 +1,77 @@
+-- Copyright (c) 2012 Jonas Tullus
+-- Licensed under MIT license
+-- Available on GitHub at http://github.com/jethr0/Countdown
+-- You can find the task at: http://www.rubyquiz.com/quiz7.html
+--
+-- Program finds the first exactly matching solution
+module Main where
+
+import Control.Monad (guard)
+import Data.List (delete, sort)
+
+type NumType = Rational
+data Term = Plus  {arg1, arg2 :: Term,  eval :: NumType}
+          | Minus {arg1, arg2 :: Term,  eval :: NumType}
+          | Mult  {arg1, arg2 :: Term,  eval :: NumType}
+          | Div   {arg1, arg2 :: Term,  eval :: NumType}
+          | TNum  {                     eval :: NumType}
+          deriving (Eq, Ord)
+
+
+instance Show Term where
+  show (TNum  a)      = show $ (round a :: Int)
+  show (Plus  a b _)  = show' "+" a b
+  show (Minus a b _)  = show' "-" a b
+  show (Mult  a b _)  = show' "*" a b
+  show (Div   a b _)  = show' "/" a b
+show' op a b  = "(" ++ show a ++ op ++ show b ++ ")"
+
+
+tPlus  a b  = Plus  a b (eval a + eval b)
+tMinus a b  = Minus a b (eval a - eval b)
+tMult  a b  = Mult  a b (eval a * eval b)
+tDiv   a b  = Div   a b (eval a / eval b)
+tNum   a    = TNum  a
+
+
+-- (++) and delete are incredibly slow operations
+-- we could improve speed by a large factor using a better data type.
+generateTerms ns = generateTerms' (length ns) (sort ns)
+generateTerms' l ns = takeNumber ++ pluss ++ minuss ++ mults ++ divs
+  where takeNumber = do
+          n <- ns
+          return (delete n ns, TNum n)
+        getArgs = do
+          guard (l >= 2)
+          (ns' , left)  <- generateTerms' (l-1) ns
+          (ns'', right) <- generateTerms' (l-1) ns'
+          return (ns'', left, right)
+        pluss = do
+          (ns'', left, right) <- getArgs
+          guard (eval left < eval right)
+          guard (eval left /= 0 && eval right /= 0)
+          return $ (ns'', tPlus left right)
+        minuss = do
+          (ns'', left, right) <- getArgs
+          guard (eval right /= 0)
+          return $ (ns'', tMinus left right)
+        mults = do
+          (ns'', left, right) <- getArgs
+          guard (eval left < eval right)
+          guard (eval left /= 1 && eval right /= 1)
+          return $ (ns'', tMult left right)
+        divs = do
+          (ns'', left, right) <- getArgs
+          guard (eval right /= 0 && eval right /= 1)
+          return $ (ns'', tDiv left right)
+
+
+run ns target = filter pred $ generateTerms ns
+  where pred (_, t) = eval t == target
+
+
+numbers = [100, 5, 5, 2, 6, 8] :: [NumType]
+
+
+main = 
+  putStrLn . unlines . map (show . snd) . take 1 $ run numbers 522
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-Countdown
-=========
-
 Haskell Uncut - Ruby Quiz #7 - Countdown
+========================================
+
+Result of me live-coding [Ruby Quiz #7](http://www.rubyquiz.com/quiz7.html) - Implement the "numbers round" game from the UK quiz show "Countdown".
+
+You can see the video at [Haskell Uncut](http://youtube.com/user/entirelysubjective)
+
+This took me so much longer than I had hoped. I guess I was just a little tired after a hard days work.
+
+This solution takes ca. 0.4s under VirtualBox to find the first exact solution. 
+
+It could be faster by a great factor if I used a better suited data type. Right now I am using (++) and Data.List.delete.
